fa83da12cf59a23992852b8a9c84a06de54f26b5
Moyeong Lee <2000prcs@gmail.com>
2018-08-07 23:49:05 -0700

    Add command handlers

---
+++ commands.js
@@ -0,0 +1,56 @@
+
+let companyNames;
+// let companyNumber;
+
+// Handle each commands 
+const Commands = {
+  locate: (companies, arg) => {
+    companyNames = companies
+      .filter(company => company.state === arg)
+      .map(company => company.company_name);
+    // console.log(companyNames);
+    // console.log(companyNames.length);
+    return companyNames;
+  },
+  find_before: (companies, arg) => {
+    companyNames = companies
+      .filter(company => {
+        return company.year_founded && company.year_founded <= arg
+      })
+      .map(company => company.company_name);   
+    // console.log(companyNames);
+    // console.log(companyNames.length);
+    return companyNames;
+  },
+  find_after: (companies, arg) => {
+    companyNames = companies
+      .filter(company => {
+        return company.year_founded && company.year_founded >= arg
+      })
+      .map(company => company.company_name);
+    // console.log(companyNames);
+    // console.log(companyNames.length);
+    return companyNames;
+  },
+  find_companies_between_size: (companies, arg) => {
+    companyNames = companies
+      .filter(company => company.full_time_employees === arg)
+      .map(company => company.company_name);
+    // console.log(companyNames);
+    // console.log(companyNames.length);
+    return companyNames;
+  },
+  find_type: (companies, arg) => {
+    companyNames = companies
+      .filter(company => company.company_category === arg)
+      .map(company => company.company_name);
+    // console.log(companyNames);
+    // console.log(companyNames.length);
+    return companyNames;
+  }
+}
+
+
+module.exports = {
+  Commands,
+};
--- index.js
+++ index.js
@@ -2,21 +2,34 @@
 
 const fs = require('fs');
 const path = require('path');
+const { Commands } = require('./commands');
 
-const [,, args] = process.argv;
-console.log(process.argv)
-console.log(args);
+const [,, ...args] = process.argv;
+// console.log(process.argv)
+// console.log(args);
 
+// Prints output to STDOUT
+const logCompanies = (data) => {
+  if(data.length === 0){
+    console.log('No data found');
+    return;
+  } 
+  console.log(`Company Names:\n${data.join(', ')}\n\nNumber of Companies: ${data.length}`);
+}
+
+
+// Main function to take input from STDIN
 const miner = (file, command, argument) => {
   
   let data = fs.readFileSync(path.resolve(file), 'utf8');
-  console.log(data);
-  
+  data = JSON.parse(data);
+
+  // Call command handlers 
+  let result = Commands[command](data, argument);
+  logCompanies(result);
 }
 
-miner(args);
 
-// process.stidin.on('data', data => {
-//   console.log(data);
 
-// });
+miner(args[0], args[1], args[2]);
+
